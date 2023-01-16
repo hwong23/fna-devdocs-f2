21b28ce04612095b94e0a83ca644553e0cee8910
hwong23 <hwong23@gmail.com>
2023-01-15 20:14:49 -0500

    manuscr

--- build/build.sh
+++ build/build.sh
@@ -27,8 +27,12 @@ COMMIT="${TRIGGERING_SHA_7}"
 # Pandoc's configuration is specified via files of option defaults
 # located in the $PANDOC_DATA_DIR/defaults directory.
 PANDOC_DATA_DIR="${PANDOC_DATA_DIR:-build/pandoc}"
-
 export FECHA_COMPILACION COMMIT
+
+# Add commit hash to the manuscript
+envsubst < output/manuscript.md > output/manuscript.hash
+mv output/manuscript.hash output/manuscript.md
+
 
 
 # Generate reference information
